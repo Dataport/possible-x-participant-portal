972e56cb7907ffe566d5fb4e87c9e957ef84fd0e
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-08-27 16:45:55 +0200

    Add ProviderServiceFake for tests

--- backend/src/test/java/eu/possiblex/participantportal/application/boundary/ProviderRestApiTest.java
+++ backend/src/test/java/eu/possiblex/participantportal/application/boundary/ProviderRestApiTest.java
@@ -6,25 +6,22 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import eu.possiblex.participantportal.application.control.ProviderApiMapper;
 import eu.possiblex.participantportal.application.entity.CreateOfferRequestTO;
 import eu.possiblex.participantportal.business.control.ProviderService;
+import eu.possiblex.participantportal.business.control.ProviderServiceFake;
 import eu.possiblex.participantportal.business.entity.edc.CreateEdcOfferBE;
-import eu.possiblex.participantportal.business.entity.edc.common.IdResponse;
 import eu.possiblex.participantportal.business.entity.fh.CreateDatasetEntryBE;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mapstruct.factory.Mappers;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -34,14 +31,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ContextConfiguration(classes = { ProviderRestApiTest.TestConfig.class, ProviderRestApiImpl.class })
 class ProviderRestApiTest {
 
-    private static final String ASSET_NAME = "BestAsset3000";
-
-    private static final String CREATE_OFFER_RESPONSE_ID = "abc123";
-
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @Autowired
     private ProviderService providerService;
 
     public static String asJsonString(final Object obj) {
@@ -51,15 +44,6 @@ class ProviderRestApiTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-
-        IdResponse createOfferResponse = new IdResponse();
-        createOfferResponse.setId(CREATE_OFFER_RESPONSE_ID);
-        lenient().when(providerService.createOffer(any(), any())).thenReturn(createOfferResponse);
-
     }
 
     @Test
@@ -75,7 +59,7 @@ class ProviderRestApiTest {
         //then
         this.mockMvc.perform(post("/provider/offer").content(asJsonString(request))
                 .contentType(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isOk())
-            .andExpect(jsonPath("$.id").value(CREATE_OFFER_RESPONSE_ID));
+            .andExpect(jsonPath("$.id").value(ProviderServiceFake.CREATE_OFFER_RESPONSE_ID));
 
         ArgumentCaptor<CreateDatasetEntryBE> createDatasetEntryCaptor = ArgumentCaptor.forClass(
             CreateDatasetEntryBE.class);
@@ -93,6 +77,11 @@ class ProviderRestApiTest {
 
     @TestConfiguration
     static class TestConfig {
+        @Bean
+        public ProviderService providerService() {
+            return Mockito.spy(new ProviderServiceFake());
+        }
+
         @Bean
         public ProviderApiMapper providerApiMapper() {
 
