eceecc4adfc265ac52832e8541f0cf1d6cd1ab39
M-Busk <125284318+M-Busk@users.noreply.github.com>
2024-09-18 11:11:02 +0200

    remove forced checkbox for booleans if minCount > 1

--- frontend/src/app/sdwizard/core/dynamic-form/services/validation.service.ts
+++ frontend/src/app/sdwizard/core/dynamic-form/services/validation.service.ts
@@ -49,13 +49,9 @@ export class ValidationControlService {
         validator.push(specificValidation);
       }
     });
-    
+
     if (input.required) {
-      if(input.datatype.value === 'boolean' ) {
-        validator.push(Validators.requiredTrue);
-      } else {
-        validator.push(Validators.required);
-      }
+      validator.push(Validators.required);
     }
     if(typeof input.datatype.value === 'string' )
     {
