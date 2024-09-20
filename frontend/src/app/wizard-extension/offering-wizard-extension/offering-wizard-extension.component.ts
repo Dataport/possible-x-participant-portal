e93d90628e1b221690908ce76668dcac6f65a5e6
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-09-20 14:51:19 +0200

    Add logic to exclude fields from the shapes

--- frontend/src/app/wizard-extension/offering-wizard-extension/offering-wizard-extension.component.ts
+++ frontend/src/app/wizard-extension/offering-wizard-extension/offering-wizard-extension.component.ts
@@ -14,15 +14,19 @@
  *  limitations under the License.
  */
 
-import { Component, ViewChild } from '@angular/core';
-import { StatusMessageComponent } from '../../views/common-views/status-message/status-message.component';
-import { BaseWizardExtensionComponent } from '../base-wizard-extension/base-wizard-extension.component';
-import { isGxServiceOfferingCs, isDataResourceCs } from '../../utils/credential-utils';
-import { BehaviorSubject, takeWhile } from 'rxjs';
+import {Component, ViewChild} from '@angular/core';
+import {StatusMessageComponent} from '../../views/common-views/status-message/status-message.component';
+import {BaseWizardExtensionComponent} from '../base-wizard-extension/base-wizard-extension.component';
+import {isDataResourceCs, isGxServiceOfferingCs} from '../../utils/credential-utils';
+import {BehaviorSubject, takeWhile} from 'rxjs';
 import {HttpErrorResponse} from '@angular/common/http';
-import { ApiService } from '../../services/mgmt/api/api.service';
-import { POLICY_MAP } from '../../constants';
-import { IGxDataResourceCredentialSubject, IGxServiceOfferingCredentialSubject, IPojoCredentialSubject } from '../../services/mgmt/api/backend';
+import {ApiService} from '../../services/mgmt/api/api.service';
+import {POLICY_MAP} from '../../constants';
+import {
+  IGxDataResourceCredentialSubject,
+  IGxServiceOfferingCredentialSubject,
+  IPojoCredentialSubject
+} from '../../services/mgmt/api/backend';
 
 @Component({
   selector: 'app-offering-wizard-extension',
@@ -30,47 +34,47 @@ import { IGxDataResourceCredentialSubject, IGxServiceOfferingCredentialSubject, 
   styleUrls: ['./offering-wizard-extension.component.scss']
 })
 export class OfferingWizardExtensionComponent {
-  @ViewChild("gxServiceOfferingWizard") private gxServiceOfferingWizard: BaseWizardExtensionComponent;
-  @ViewChild("gxDataResourceWizard") private gxDataResourceWizard: BaseWizardExtensionComponent;
   @ViewChild("offerCreationStatusMessage") public offerCreationStatusMessage!: StatusMessageComponent;
-
   selectedFileName: string = "";
   policyMap = POLICY_MAP;
   selectedPolicy: string = "";
-
-
   public prefillDone: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
-
   protected isDataOffering: boolean = true;
+  @ViewChild("gxServiceOfferingWizard") private gxServiceOfferingWizard: BaseWizardExtensionComponent;
+  @ViewChild("gxDataResourceWizard") private gxDataResourceWizard: BaseWizardExtensionComponent;
 
   constructor(
     private apiService: ApiService
-  ) {}
+  ) {
+  }
+
+  get isInvalidFileName(): boolean {
+    return !this.isFieldFilled(this.selectedFileName);
+  }
+
+  get isInvalidPolicy(): boolean {
+    return !this.isFieldFilled(this.selectedPolicy);
+  }
 
   public async loadShape(offerType: string, serviceOfferingId: string, dataResourceId: string): Promise<void> {
     this.isDataOffering = offerType === "data";
-    
-    this.prefillDone.next(false);
-    console.log("Loading service offering shape"); 
-    await this.gxServiceOfferingWizard.loadShape(this.apiService.getGxServiceOfferingShape(), serviceOfferingId);
 
-    if(this.isOfferingDataOffering()) {
-      console.log("Loading data resource shape"); 
-      await this.gxDataResourceWizard.loadShape(this.apiService.getGxDataResourceShape(), dataResourceId);
+    this.prefillDone.next(false);
+    console.log("Loading service offering shape");
+    let serviceOfferingShapeSource = await this.apiService.getGxServiceOfferingShape();
+    serviceOfferingShapeSource = this.adaptGxShape(serviceOfferingShapeSource, "ServiceOffering", ["policy", "aggregationOf"]);
+    await this.gxServiceOfferingWizard.loadShape(Promise.resolve(serviceOfferingShapeSource), serviceOfferingId);
+
+    if (this.isOfferingDataOffering()) {
+      console.log("Loading data resource shape");
+      let dataResourceShapeSource = await this.apiService.getGxDataResourceShape();
+      dataResourceShapeSource = this.adaptGxShape(dataResourceShapeSource, "DataResource", ["name", "description", "policy"]);
+      await this.gxDataResourceWizard.loadShape(Promise.resolve(dataResourceShapeSource), dataResourceId);
     }
   }
 
   public isShapeLoaded(): boolean {
     return this.gxServiceOfferingWizard?.isShapeLoaded() && this.isOfferingDataOffering() ? this.gxDataResourceWizard?.isShapeLoaded() : true;
-  }
-
-  private prefillHandleCs(cs: IPojoCredentialSubject) {
-    if (isGxServiceOfferingCs(cs)) {
-      this.gxServiceOfferingWizard.prefillFields(cs, ["gx:providedBy"]);
-    }
-    if (isDataResourceCs(cs)) {
-      this.gxDataResourceWizard.prefillFields(cs, ["gx:producedBy", "gx:exposedThrough", "gx:copyrightOwnedBy"]);
-    }
   }
 
   public prefillFields(csList: IPojoCredentialSubject[]) {
@@ -80,32 +84,32 @@ export class OfferingWizardExtensionComponent {
 
     if (!this.isOfferingDataOffering()) {
       this.gxServiceOfferingWizard.prefillDone
-      .pipe(
-        takeWhile(done => !done, true)
-      )
-      .subscribe(done => {
-        if (done) {
-          this.prefillDone.next(true);
-        }
-      });
+        .pipe(
+          takeWhile(done => !done, true)
+        )
+        .subscribe(done => {
+          if (done) {
+            this.prefillDone.next(true);
+          }
+        });
     } else {
       this.gxServiceOfferingWizard.prefillDone
-      .pipe(
-        takeWhile(done => !done, true)
-      )
-      .subscribe(done => {
-        if (done) {
-          this.gxDataResourceWizard.prefillDone
-          .pipe(
-            takeWhile(done => !done, true)
-          )
-          .subscribe(done => {
-            if (done) {
-              this.prefillDone.next(true);
-            }
-          })
-        }
-      });
+        .pipe(
+          takeWhile(done => !done, true)
+        )
+        .subscribe(done => {
+          if (done) {
+            this.gxDataResourceWizard.prefillDone
+              .pipe(
+                takeWhile(done => !done, true)
+              )
+              .subscribe(done => {
+                if (done) {
+                  this.prefillDone.next(true);
+                }
+              })
+          }
+        });
     }
   }
 
@@ -116,11 +120,11 @@ export class OfferingWizardExtensionComponent {
     let gxOfferingJsonSd: IGxServiceOfferingCredentialSubject = this.gxServiceOfferingWizard.generateJsonCs();
 
     let createOfferTo: any = {
-        credentialSubjectList: [
-          gxOfferingJsonSd,
-        ],
-        fileName: this.selectedFileName,
-        policy: this.policyMap[this.selectedPolicy].policy
+      credentialSubjectList: [
+        gxOfferingJsonSd,
+      ],
+      fileName: this.selectedFileName,
+      policy: this.policyMap[this.selectedPolicy].policy
     }
 
     if (this.isOfferingDataOffering()) {
@@ -129,7 +133,7 @@ export class OfferingWizardExtensionComponent {
     }
 
     console.log(createOfferTo);
-    
+
     this.apiService.createOffer(createOfferTo).then(response => {
       console.log(response);
       this.offerCreationStatusMessage.showSuccessMessage("", 20000);
@@ -138,7 +142,45 @@ export class OfferingWizardExtensionComponent {
     }).catch(_ => {
       this.offerCreationStatusMessage.showErrorMessage("Unbekannter Fehler");
     });
-    
+
+  }
+
+  public ngOnDestroy() {
+    this.gxServiceOfferingWizard.ngOnDestroy();
+    this.gxDataResourceWizard.ngOnDestroy();
+    this.resetPossibleSpecificFormValues();
+    this.offerCreationStatusMessage.hideAllMessages();
+  }
+
+  public isFieldFilled(str: string) {
+    if (!str || str.trim().length === 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public resetPossibleSpecificFormValues() {
+    this.selectedFileName = "";
+    this.selectedPolicy = "";
+  }
+
+  protected adaptGxShape(shapeSource: any, shapeName: string, excludedFields: string[]) {
+    if (typeof shapeSource !== 'object' || shapeSource === null) {
+      console.error("Invalid input: shape is not of expected type.");
+      return null;
+    }
+
+    shapeSource.shapes.forEach((shape: any) => {
+      if (shape.targetClassName === shapeName) {
+        shape.constraints = shape.constraints.filter((constraint: any) => {
+          return !(constraint.path.prefix === "gx" && excludedFields.includes(constraint.path.value));
+        });
+      }
+    });
+
+    console.log(shapeSource);
+    return shapeSource;
   }
 
   protected getPolicyNames() {
@@ -150,17 +192,10 @@ export class OfferingWizardExtensionComponent {
     return policyDetails ? JSON.stringify(policyDetails, null, 2) : '';
   }
 
-  public ngOnDestroy() {
-    this.gxServiceOfferingWizard.ngOnDestroy();
-    this.gxDataResourceWizard.ngOnDestroy();
-    this.resetPossibleSpecificFormValues();
-    this.offerCreationStatusMessage.hideAllMessages();
-  }
-
   protected isWizardFormInvalid(): boolean {
     let serviceOfferingWizardInvalid = this.gxServiceOfferingWizard?.isWizardFormInvalid();
     let dataResourceWizardInvalid = this.isOfferingDataOffering() ? this.gxDataResourceWizard?.isWizardFormInvalid() : false;
-  
+
     return serviceOfferingWizardInvalid || dataResourceWizardInvalid;
   }
 
@@ -168,28 +203,18 @@ export class OfferingWizardExtensionComponent {
     return this.isDataOffering;
   }
 
-  public isFieldFilled(str: string){
-    if (!str || str.trim().length === 0) {
-      return false;
-    }
-
-    return true;
-  }
-
   protected isPossibleSpecificFormInvalid(): boolean {
-    return this.isInvalidFileName || this.isInvalidPolicy;
+    let serviceOfferingPossibleSpecificFormInvalid = this.isInvalidPolicy;
+    let dataResourcePossibleSpecificFormInvalid = this.isOfferingDataOffering() ? this.isInvalidFileName : false;
+    return serviceOfferingPossibleSpecificFormInvalid || dataResourcePossibleSpecificFormInvalid;
   }
 
-  get isInvalidFileName(): boolean {
-    return !this.isFieldFilled(this.selectedFileName);
-  }
-
-  get isInvalidPolicy(): boolean {
-    return !this.isFieldFilled(this.selectedPolicy);
-  }
-
-  public resetPossibleSpecificFormValues() {
-    this.selectedFileName = "";
-    this.selectedPolicy = "";
+  private prefillHandleCs(cs: IPojoCredentialSubject) {
+    if (isGxServiceOfferingCs(cs)) {
+      this.gxServiceOfferingWizard.prefillFields(cs, ["gx:providedBy"]);
+    }
+    if (isDataResourceCs(cs)) {
+      this.gxDataResourceWizard.prefillFields(cs, ["gx:producedBy", "gx:exposedThrough", "gx:copyrightOwnedBy"]);
+    }
   }
 }
