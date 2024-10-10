79871c4db86c8b2a6843bac64bd855dd0bb4c364
Andreas Neufeld <andreas.neufeld@capgemini.com>
2024-10-10 11:19:27 +0200

    create tag contracts

---
+++ frontend/src/app/views/contracts/contracts.component.ts
@@ -0,0 +1,15 @@
+// import { Component } from '@angular/core';
+// import { IOfferDetailsTO } from '../../../services/mgmt/api/backend';
+//
+// @Component({
+//   selector: 'app-consume',
+//   templateUrl: './contracts.component.html',
+//   styleUrls: ['./contracts.component.scss']
+// })
+// export class ContractsComponent {
+//   selectedOffer?: IOfferDetailsTO = undefined;
+//
+//   setSelectedOffer(offer: IOfferDetailsTO | undefined): void {
+//     this.selectedOffer = offer;
+//   }
+// }
