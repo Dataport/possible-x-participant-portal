62f42fcab2cc789d1cbd3d11795f8fba06c9f504
usulsk <skobilke@gmail.com>
2024-10-27 10:49:15 +0100

    PI-565: full module test for create service offer

---
+++ localdevelopment/build.bat
@@ -0,0 +1,24 @@
+@ECHO OFF
+SETLOCAL enableextensions enabledelayedexpansion
+
+SET PATH=C:\Projects\POSSIBLE\tools\jdk17\bin
+
+SET fh.catalog.url=http://localhost:9090
+SET edc.mgmt-base-url=http://localhost:9090
+
+ECHO.
+ECHO build and run
+ECHO.
+
+
+CD..
+
+call gradlew.bat backend:cleanBuild
+
+ECHO.
+ECHO DONE
+ECHO.
+ECHO.
+PAUSE
+ECHO.
+
