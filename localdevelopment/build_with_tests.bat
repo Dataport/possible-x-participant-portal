680c47c5dc3ee708940a151ad14e96b5ecfd7e88
usulsk <skobilke@gmail.com>
2024-10-27 10:58:33 +0100

    PI-565: helper scripts

--- localdevelopment/build_with_tests.bat
+++ localdevelopment/build_with_tests.bat
@@ -3,17 +3,15 @@ SETLOCAL enableextensions enabledelayedexpansion
 
 SET PATH=C:\Projects\POSSIBLE\tools\jdk17\bin
 
-SET fh.catalog.url=http://localhost:9090
-SET edc.mgmt-base-url=http://localhost:9090
-
 ECHO.
-ECHO build and run
+ECHO build WITH tests
 ECHO.
 
 
 CD..
 
 call gradlew.bat backend:cleanBuild
+call gradlew.bat backend:clean test --info
 
 ECHO.
 ECHO DONE
