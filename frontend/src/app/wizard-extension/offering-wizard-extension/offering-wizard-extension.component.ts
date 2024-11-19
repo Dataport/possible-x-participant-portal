bebb9623b0741cad5529771b412b4ec5f0d5145b
asennes <alexandra.sennes@capgemini.com>
2024-11-19 16:20:21 +0100

    PI-652 resolve conflicts

--- frontend/src/app/wizard-extension/offering-wizard-extension/offering-wizard-extension.component.ts
+++ frontend/src/app/wizard-extension/offering-wizard-extension/offering-wizard-extension.component.ts
@@ -49,6 +49,7 @@ export class OfferingWizardExtensionComponent implements AfterViewInit {
   dataResourceShapeSource = "";
   @ViewChild("stepper") stepper: MatStepper;
   @ViewChild('accordionItem') accordionItem!: AccordionItemComponent;
+  protected isDataOffering: boolean = true;
   @ViewChild("gxServiceOfferingWizard") private gxServiceOfferingWizard: BaseWizardExtensionComponent;
   @ViewChild("gxDataResourceWizard") private gxDataResourceWizard: BaseWizardExtensionComponent;
 
