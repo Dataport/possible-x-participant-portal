ae912b5ae864224124ecbc5b4dd862a45f336635
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2024-11-20 16:28:39 +0100

    PI-679, PI-682: Adapt consumer rest api mapper

--- backend/src/main/java/eu/possiblex/participantportal/application/control/ConsumerApiMapper.java
+++ backend/src/main/java/eu/possiblex/participantportal/application/control/ConsumerApiMapper.java
@@ -1,11 +1,17 @@
 package eu.possiblex.participantportal.application.control;
 
 import eu.possiblex.participantportal.application.entity.*;
+import eu.possiblex.participantportal.application.entity.policies.EnforcementPolicy;
+import eu.possiblex.participantportal.application.entity.policies.EverythingAllowedPolicy;
 import eu.possiblex.participantportal.business.entity.*;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Mapper(componentModel = "spring", imports = { OffsetDateTime.class })
 public interface ConsumerApiMapper {
@@ -28,9 +34,24 @@ public interface ConsumerApiMapper {
     TransferOfferResponseTO transferOfferResponseBEToTransferOfferResponseTO(
         TransferOfferResponseBE transferOfferResponseBE);
 
-    @Mapping(target = "consumerDetails.participantId", source = "consumer.id")
+    @Mapping(target = "consumerDetails.participantId", source = "consumer.id", qualifiedByName = "extractParticipantDidWeb")
     @Mapping(target = "consumerDetails.participantName", source = "consumer.name")
-    @Mapping(target = "providerDetails.participantId", source = "provider.id")
+    @Mapping(target = "providerDetails.participantId", source = "provider.id", qualifiedByName = "extractParticipantDidWeb")
     @Mapping(target = "providerDetails.participantName", source = "provider.name")
     ContractPartiesTO contractPartiesBEToContractPartiesTO(ContractPartiesBE contractPartiesBE);
+
+    @Named("extractParticipantDidWeb")
+    default String extractParticipantDid(String id) {
+
+        String didWebRegex = "did:web.*";
+
+        Pattern pattern = Pattern.compile(didWebRegex);
+        Matcher matcher = pattern.matcher(id);
+
+        String extracted = "";
+        if (matcher.find()) {
+            extracted = matcher.group();
+        }
+        return extracted;
+    }
 }
