7afb51830b2ddfec5e92f5c21b454e3f82ff6b69
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2025-01-13 13:45:03 +0100

    PI-834: Add/adapt tests in UI

--- frontend/src/app/views/offer/enforcement-policy-accordion-view/enforcement-policy-accordion-view.component.spec.ts
+++ frontend/src/app/views/offer/enforcement-policy-accordion-view/enforcement-policy-accordion-view.component.spec.ts
@@ -8,7 +8,12 @@ import {MatExpansionModule} from '@angular/material/expansion';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import {NameMappingService} from "../../../services/mgmt/name-mapping.service";
+import {
+  IEndDatePolicy,
+  IEnforcementPolicy,
+  IParticipantRestrictionPolicy
+} from "../../../services/mgmt/api/backend";
+import {EnforcementPolicyContentComponent} from "./enforcement-policy-content/enforcement-policy-content.component";
 
 describe('EnforcementPolicyViewComponent', () => {
   let component: EnforcementPolicyAccordionViewComponent;
@@ -16,9 +21,8 @@ describe('EnforcementPolicyViewComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [EnforcementPolicyAccordionViewComponent],
-      imports: [MatExpansionModule, BrowserAnimationsModule, HttpClientTestingModule],
-      providers: [NameMappingService]
+      declarations: [EnforcementPolicyAccordionViewComponent, EnforcementPolicyContentComponent],
+      imports: [MatExpansionModule, BrowserAnimationsModule, HttpClientTestingModule]
     });
     fixture = TestBed.createComponent(EnforcementPolicyAccordionViewComponent);
     component = fixture.componentInstance;
@@ -27,5 +31,43 @@ describe('EnforcementPolicyViewComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set input properties correctly', () => {
+    const policies: IEnforcementPolicy[] = [{ "@type": "EverythingAllowedPolicy", "valid": true }];
+    component.enforcementPolicies = policies;
+    component.showValidity = true;
+    fixture.detectChanges();
+
+    expect(component.enforcementPolicies).toBe(policies);
+    expect(component.showValidity).toBeTrue();
+  });
+
+  it('should detect invalid policies', () => {
+    component.enforcementPolicies = [
+      {"@type": "EndDatePolicy", "valid": false, "date": new Date()} as IEndDatePolicy,
+      {
+        "@type": "ParticipantRestrictionPolicy",
+        "valid": true,
+        "allowedParticipants": ["org1", "org2"]
+      } as IParticipantRestrictionPolicy
+    ];
+    fixture.detectChanges();
+
+    expect(component.isAnyPolicyInvalid).toBeTrue();
+  });
+
+  it('should detect no invalid policies', () => {
+    component.enforcementPolicies = [
+      {"@type": "EndDatePolicy", "valid": true, "date": new Date()} as IEndDatePolicy,
+      {
+        "@type": "ParticipantRestrictionPolicy",
+        "valid": true,
+        "allowedParticipants": ["org1", "org2"]
+      } as IParticipantRestrictionPolicy
+    ];
+    fixture.detectChanges();
+
+    expect(component.isAnyPolicyInvalid).toBeFalse();
   });
 });
