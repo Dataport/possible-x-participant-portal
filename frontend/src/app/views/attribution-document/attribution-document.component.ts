1b148c9d0315080268c34a82b08dd976116244fe
Kim Chi Tran <146179578+kimchitran-cap@users.noreply.github.com>
2025-02-03 11:23:32 +0100

    Use routerlink with fragment

--- frontend/src/app/views/attribution-document/attribution-document.component.ts
+++ frontend/src/app/views/attribution-document/attribution-document.component.ts
@@ -1,14 +1,9 @@
 import { Component } from '@angular/core';
-import {ViewportScroller} from "@angular/common";
 
 @Component({
   templateUrl: './attribution-document.component.html',
   styleUrls: ['./attribution-document.component.scss']
 })
 export class AttributionDocumentComponent {
-  constructor(private readonly viewportScroller: ViewportScroller) {}
 
-  public onClick(elementId: string): void {
-    this.viewportScroller.scrollToAnchor(elementId);
-  }
 }
